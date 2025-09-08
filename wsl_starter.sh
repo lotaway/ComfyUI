787125729db69cfcf93375b2e7c70e96e0947e3e
lotaway <576696294@qq.com>
2025-09-08 17:05:04 +0800

    Update wsl shell.

--- wsl_starter.sh
+++ wsl_starter.sh
@@ -1,3 +1,5 @@
-# Nedd to create python venv manually
+#!/bin/bash
+# Nedd to create python venv manuallys
 source venv/bin/activate
-python main.py --lowvram --reserve-vram=3.8
+python main.py --listen --lowvram --reserve-vram=3.8
+#deactivate
