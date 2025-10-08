795445d0be494b9e5a2bad2cf30730b674a3ff53
lotaway <wayluk@lotaway.>
2025-10-08 22:11:22 +0800

    Fix shell and requiremnet error.

--- wsl_starter.sh
+++ wsl_starter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Nedd to create python venv manuallys
-source venv/bin/activate
-python main.py --listen --lowvram --reserve-vram=3.8
+mamba run -n python3.12 python main.py --listen --lowvram --reserve-vram=3.8 --enable-cors-header="*"
 #deactivate
