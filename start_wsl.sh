990116d35a8eee7a2b999a01fa5e07ae112c1e5d
lotaway <576696294@qq.com>
2025-11-08 20:32:33 +0800

    Add wsl start command.

---
+++ start_wsl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mamba run -n python3.12 python main.py --listen --reserve-vram=3.8
+#deactivate
--- wsl_starter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Nedd to create python venv manuallys
-mamba run -n python3.12 python main.py --listen --lowvram --reserve-vram=3.8 --enable-cors-header="*"
-#deactivate
